e0623c355dfcb05d77c80293dedb6b187900adab
AndreasBGJensen <bgandreas@hotmail.com>
2020-02-14 21:17:57 +0100

    Add:

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import Controller.GameController.GameController;
+import Controller.GameController.GameControllerModserver;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            GameController play = new GameController();
+            GameControllerModserver playDTUServer = new GameControllerModserver();
+            //GameController play = new GameController();
         }catch (Exception e){
             e.printStackTrace();
         }
--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -11,11 +11,12 @@ public class ServerMain {
         public static void main(String[] arg) throws Exception
         {
             // Enten: Kør programmet 'rmiregistry' fra mappen med .class-filerne, eller:
-            System.setProperty("java.rmi.server.hostname", "javabog.dk");
+            System.setProperty("dist.saluton.dk", "kontotjeneste");
             java.rmi.registry.LocateRegistry.createRegistry(1099); // start i server-JVM
 
             IPlayGalgeleg k = new Server.JavaSkelImpl();
-            Naming.rebind("rmi://localhost/kontotjeneste", k);
+            Naming.rebind("rmi://java.rmi.server.hostname/kontotjeneste", k);
+           // Naming.rebind("rmi://localhost/kontotjeneste", k);
             System.out.println("Kontotjeneste registreret.");
         }
     }
